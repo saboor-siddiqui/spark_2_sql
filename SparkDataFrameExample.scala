630ea58c408f407f59e49b727a6aca34b9fdec2d
Saboor Siddiqui <saboor.siddiqui0@gmail.com>
2025-01-17 18:18:15 +0530

    Changes for Level 2 Complexity Spark DataFrame Code

---
+++ SparkDataFrameExample.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+
+object SparkDataFrameExample {
+  def main(spark: SparkSession): Unit = {
+    val df = spark.read.table("sales")
+    
+    // Basic select and filter
+    val result1 = df.select("product_id", "amount")
+      .filter("amount > 1000")
+      
+    // Join with aggregation
+    val result2 = df.select("sales.date", "customers.name") 
+      .join("customers", "sales.customer_id = customers.id")
+      .groupBy("date")
+      .agg(sum("amount").as("total_sales"))
+      .orderBy(desc("total_sales"))
+      .limit(10)
+      
+    // Window function
+    val windowSpec = Window
+      .partitionBy("product_id")
+      .orderBy(col("amount").desc)
+      
+    val result3 = df.select("product_id", "amount")
+      .withColumn("rank", rank().over(windowSpec))
+      .filter("rank <= 3")
+      .distinct()
+  }
+}
